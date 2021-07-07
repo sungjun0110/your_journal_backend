8d5fb2e3b13efd3226cab26f0f935844ac25c1b0
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-07-06 22:12:34 -0700

    update get endpoint

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.get("/journals", async (req, res) => {
     return;
   }
   const { journals } = await Journal.findOne({ author: username }).exec();
-  res.json(journals.reverse());
+  res.json(journals);
 });
 
 app.delete("/journals", async (req, res) => {
